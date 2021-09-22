f39601aa8ccd8c1f7a1feed59b5558afd065ec85
SlavaDementev <kursant344@gmail.com>
2021-09-22 23:04:18 +0300

    some change

--- src/main/kotlin/ru/netology/Main.kt
+++ src/main/kotlin/ru/netology/Main.kt
@@ -2,13 +2,12 @@ package ru.netology
 
 fun main() {
 
-    val card = 14000_00
     val transfer = 100_00
     val commission = transfer * 75 / 1000
     val amount = if (commission > 35_00) {
-        card - transfer - commission
+        transfer - commission
     } else {
-        card - transfer - 35
+        transfer - 35
     }
     print(amount)
 }
